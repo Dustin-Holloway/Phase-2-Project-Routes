8b3a7ac1d34b1087ae08ee2de28159c4780a5b3b
CahenTaylor <cahentaylor@gmail.com>
2023-04-25 13:58:06 -0600

    AddToList box basic functions

--- routes-app/src/tripform.js
+++ routes-app/src/tripform.js
@@ -1,23 +1,40 @@
-import React {useState} from "react"
+import React, {useState} from "react"
 
 export default function TripForm(){
 
 
+    const [addNotes, setAddNotes] = useState("");
+
+    function handleSubmit(e) {
+      e.preventDefault();
+      console.log(e.target.addNotes.value);
+    }
+    const [addToList, setAddToList] = useState(false)
+    function handleClick(e) {
+        e.preventDefault();
+        console.log("clicked")
+    }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="Subscribe">Subscribe</label>
-                <input
-                    onChange={(e) => setUserEmail(e.target.value)}
-                    value={userEmail}
-                    type="text"
-                    id="userEmail"
-                />
-                {/* <input></input>
+      <div className="form-div">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="addnotes">Add Notes</label>
+          <input
+            onChange={(e) => setAddNotes(e.target.value)}
+            value={addNotes}
+            type="text"
+            id="addNotes"
+          />
+          <label htmlFor="addtolist">Add To My List</label>
+          <input
+            onClick={(e) => setAddToList(e.target.clicked)}
+            type="checkbox"
+            value={addToList}
+          />
+          {/* 
                 <input></input>
                 <input></input> */}
-            </form>
-    </div>
-    )
+        </form>
+      </div>
+    );
 }
