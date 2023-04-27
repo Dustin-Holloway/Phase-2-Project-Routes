48087697ce509599f4eec64b61a7de854d8f95d2
Dustin <dustinhollowaypdx@gmail.com>
2023-04-27 08:05:57 -0700

    Thursday

--- routes-app/src/landing.js
+++ routes-app/src/landing.js
@@ -5,12 +5,14 @@ export default function Landing() {
   return (
     <div className="landing-container">
       <Link to="/gallery">
-        <h1>Let's take a trip!</h1>
+        <div className="image-container">
+          <h1>Let's take a trip!</h1>
 
-        <img
-          className="landing-image"
-          src="https://cdn.shopify.com/s/files/1/0216/7573/7188/products/nationalpark-u_1800x1800.png?v=1677435572"
-        />
+          <img
+            className="landing-image"
+            src="https://cdn.shopify.com/s/files/1/0216/7573/7188/products/nationalpark-u_1800x1800.png?v=1677435572"
+          />
+        </div>
       </Link>
     </div>
   );
