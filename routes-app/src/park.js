8f8288fe3779eeda4a02499e57a0ff7e97d2bfd1
Dustin <dustinhollowaypdx@gmail.com>
2023-04-27 15:26:59 -0700

    added links

--- routes-app/src/park.js
+++ routes-app/src/park.js
@@ -1,23 +1,34 @@
 import React from "react";
 
 export default function Park({ park }) {
-  console.log(park);
+  console.log(park.entranceFees);
 
   const activities = park.activities.map((item) => item.name);
   console.log(activities);
 
   const filteredActivities = activities.slice(0, 4);
   const listItem = filteredActivities.map((item) => <li>{item}</li>);
-
+  const admissionPrice = park.entranceFees.map((item) => item.entranceFees);
+  console.log(admissionPrice);
   return (
     <div className="park-container">
       <h1>{park.fullName}</h1>
       <div className="img-container">
-        <img src={park.images[2].url} />
+        <img src={park.images[2].url} alt={park.name} />
+        <a href={park.url}>visitors center</a>
       </div>
+      <h3>Location</h3>
+      <ul className="activities">
+        <li>{park.states}</li>
+      </ul>
       <h3>Activities</h3>
       <ul className="activities">{listItem}</ul>
-      <h3>Reviews</h3>
+      {/* <h3>Admission</h3> */}
+      {/* <ul className="activities">
+        <li>Per Person: {park.entranceFees[2].cost}</li>
+        <li>Private Vehicle: {park.entranceFees[1].cost}</li>
+        <li>Motorcycle: {park.entranceFees[0].cost}</li> */}
+      {/* </ul> */}
     </div>
   );
 }
